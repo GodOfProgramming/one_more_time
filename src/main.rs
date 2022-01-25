152a0e4855516141c21ceedfb4d037705e7e0c15
GodOfProgramming <jonathan.dickert@protonmail.com>
2022-01-24 20:20:47 -0500

    figured out how to bind functions

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,29 @@ fn main() {
 
   let isolate = &mut v8::Isolate::new(Default::default());
 
-  let scope = &mut v8::HandleScope::new(isolate);
-  let context = v8::Context::new(scope);
-  let scope = &mut v8::ContextScope::new(scope, context);
+  let handle_scope = &mut v8::HandleScope::new(isolate);
+  let context = v8::Context::new(handle_scope);
+  let scope = &mut v8::ContextScope::new(handle_scope, context);
 
-  let code = v8::String::new(scope, "'Hello' + ' ' + 'World'").unwrap();
+  fn foo(
+    a: &mut v8::HandleScope<'_>,
+    b: v8::FunctionCallbackArguments<'_>,
+    mut c: v8::ReturnValue<'_>,
+  ) {
+    let s = b.get(0);
+    println!("{}", s.to_rust_string_lossy(a));
+    let v = v8::Integer::new(a, b.length());
+    let v: v8::Local<v8::Value> = v8::Local::from(v);
+    c.set(v);
+  }
+
+  let global = context.global(scope);
+
+  let name: v8::Local<v8::Value> = v8::Local::from(v8::String::new(scope, "foo").unwrap());
+  let function: v8::Local<v8::Value> = v8::Local::from(v8::Function::new(scope, foo).unwrap());
+  global.set(scope, name, function);
+
+  let code = v8::String::new(scope, "foo('bar');").unwrap();
   println!("javascript code: {}", code.to_rust_string_lossy(scope));
 
   let script = v8::Script::compile(scope, code, None).unwrap();
